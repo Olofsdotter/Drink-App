059172675b494bd1c2624f6bec77694cdf1291a5
Hilda Olofsdotter <94hilolo@mk.se>
2020-06-22 10:10:47 +0200

    Added pages and router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import HomePage from './Components/Home/Home';
+import ExplorePage from './Components/Explore/Explore';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const DrinkHeader = () => (
+  <header>
+  <div className='drink-header'>
+  <NavLink to='/'>
+    <button>Home</button>
+  </NavLink>
+  <NavLink to='/explore'>
+    <button>Explore</button>
+  </NavLink>
+  </div>
+  </header>
+);
+
+const App = () => (
+  <BrowserRouter>
+    <div className='App'>
+    <DrinkHeader />
+    <div className='drink-content'>
+    <Switch>
+      <Route path='/' component={HomePage} exact={true} />
+      <Route path='/explore' component={ExplorePage} />
+    </Switch>
     </div>
-  );
-}
+    </div>
+  </BrowserRouter>
+);
+
 
 export default App;
---
+++ src/Components/Explore/Explore.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ExplorePage = (props) => {
+    console.log(props);
+    return <div>Explore page</div>
+};
+
+export default ExplorePage;
